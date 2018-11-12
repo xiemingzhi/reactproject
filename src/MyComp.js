611ce8668f20c3bcbee96414609341308465c3c0
ming <xiemingzhi@gmail.com>
2018-11-12 21:58:43 +0800

    Create component change initial page

---
+++ src/MyComp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class MyComp extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            <h1>helloworld</h1>
+        </div> );
+    }
+}
+ 
+export default MyComp;
